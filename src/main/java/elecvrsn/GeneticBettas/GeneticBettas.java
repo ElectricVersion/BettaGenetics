8719baecfeb9f5dd807297960168ad5282e26284
ElectricVersion <electric.version01@gmail.com>
2024-08-13 20:06:20 -0500

    added bubblenest block

--- src/main/java/elecvrsn/GeneticBettas/GeneticBettas.java
+++ src/main/java/elecvrsn/GeneticBettas/GeneticBettas.java
@@ -1,11 +1,17 @@
 package elecvrsn.GeneticBettas;
 
 import elecvrsn.GeneticBettas.init.*;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.MinecraftForge;
 
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.RegistryObject;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("geneticbettas")
@@ -18,10 +24,15 @@ public class GeneticBettas
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(instance);
 
-        AddonItems.register(FMLJavaModLoadingContext.get().getModEventBus());
-        AddonEntities.register(FMLJavaModLoadingContext.get().getModEventBus());
-        AddonSensorTypes.register(FMLJavaModLoadingContext.get().getModEventBus());
-        AddonMemoryModuleTypes.register(FMLJavaModLoadingContext.get().getModEventBus());
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        AddonBlocks.register(modEventBus);
+        AddonItems.register(modEventBus);
+        AddonEntities.register(modEventBus);
+        AddonSensorTypes.register(modEventBus);
+        AddonMemoryModuleTypes.register(modEventBus);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
     }
-
+    private void clientSetup(final FMLClientSetupEvent event) {
+        ItemBlockRenderTypes.setRenderLayer(AddonBlocks.BUBBLE_NEST.get(), RenderType.translucent());
+    }
 }
---
+++ src/main/java/elecvrsn/GeneticBettas/init/AddonBlocks.java
@@ -0,0 +1,21 @@
+package elecvrsn.GeneticBettas.init;
+
+import elecvrsn.GeneticBettas.block.BubbleNestBlock;
+import elecvrsn.GeneticBettas.util.AddonReference;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class AddonBlocks {
+    public static final DeferredRegister<Block> BLOCKS_DEFERRED_REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, AddonReference.MODID);
+    public static final RegistryObject<Block> BUBBLE_NEST = BLOCKS_DEFERRED_REGISTRY.register("bubble_nest", () -> new BubbleNestBlock(Block.Properties.of(Material.WATER, MaterialColor.ICE).noCollission().instabreak().sound(SoundType.LILY_PAD)));
+    public static void register(IEventBus modEventBus) {
+        BLOCKS_DEFERRED_REGISTRY.register(modEventBus);
+    }
+
+}
