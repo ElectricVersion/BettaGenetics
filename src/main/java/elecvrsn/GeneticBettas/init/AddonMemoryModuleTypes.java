13e7869c245679d71fabc8f13d23928c3fdff5c6
ElectricVersion <electric.version01@gmail.com>
2024-10-01 09:19:05 -0500

    removed extra-mods folder because that wasnt supposed to upload

--- src/main/java/elecvrsn/GeneticBettas/init/AddonMemoryModuleTypes.java
+++ src/main/java/elecvrsn/GeneticBettas/init/AddonMemoryModuleTypes.java
@@ -12,13 +12,14 @@ import net.minecraftforge.registries.RegistryObject;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public class AddonMemoryModuleTypes {
 
     public static final DeferredRegister<MemoryModuleType<?>> MEMORY_MODULE_TYPE_DEFERRED_REGISTRY = DeferredRegister.create(ForgeRegistries.MEMORY_MODULE_TYPES, MODID);
     public static final RegistryObject<MemoryModuleType<Boolean>> FOUND_SLEEP_SPOT = MEMORY_MODULE_TYPE_DEFERRED_REGISTRY.register("found_sleep_spot", () -> new MemoryModuleType<Boolean>(Optional.of(Codec.BOOL)));
     public static final RegistryObject<MemoryModuleType<Boolean>> MAKING_NEST = MEMORY_MODULE_TYPE_DEFERRED_REGISTRY.register("making_nest", () -> new MemoryModuleType<Boolean>(Optional.of(Codec.BOOL)));
-    public static final RegistryObject<MemoryModuleType<List<LivingEntity>>> TRUSTED_BETTAS = MEMORY_MODULE_TYPE_DEFERRED_REGISTRY.register("trusted_bettas", () -> new MemoryModuleType<List<LivingEntity>>(Optional.empty()));
+    public static final RegistryObject<MemoryModuleType<List<UUID>>> TRUSTED_BETTAS = MEMORY_MODULE_TYPE_DEFERRED_REGISTRY.register("trusted_bettas", () -> new MemoryModuleType<List<UUID>>(Optional.empty()));
     public static void register(IEventBus modEventBus) {
         MEMORY_MODULE_TYPE_DEFERRED_REGISTRY.register(modEventBus);
     }
