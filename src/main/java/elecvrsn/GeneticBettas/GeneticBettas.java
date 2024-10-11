e979460d61cdb5b37a0fe388f94c642e2eacdab9
ElectricVersion <electric.version01@gmail.com>
2024-10-11 16:56:49 -0500

    bubble nests tint to water color

--- src/main/java/elecvrsn/GeneticBettas/GeneticBettas.java
+++ src/main/java/elecvrsn/GeneticBettas/GeneticBettas.java
@@ -8,7 +8,6 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
--- src/main/java/elecvrsn/GeneticBettas/util/handlers/AddonClientEventRegistry.java
+++ src/main/java/elecvrsn/GeneticBettas/util/handlers/AddonClientEventRegistry.java
@@ -1,13 +1,17 @@
 package elecvrsn.GeneticBettas.util.handlers;
 
 
+import elecvrsn.GeneticBettas.init.AddonBlocks;
 import elecvrsn.GeneticBettas.model.ModelEnhancedBetta;
 import elecvrsn.GeneticBettas.model.ModelEnhancedBettaEgg;
 import elecvrsn.GeneticBettas.renderer.RenderEnhancedBetta;
 import elecvrsn.GeneticBettas.renderer.RenderEnhancedBettaEgg;
 import elecvrsn.GeneticBettas.util.AddonReference;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BiomeColors;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -30,6 +34,20 @@ public class AddonClientEventRegistry {
     public static void registerLayerDefinition(EntityRenderersEvent.RegisterLayerDefinitions event) {
         event.registerLayerDefinition(RenderEnhancedBetta.BETTA_LAYER, ModelEnhancedBetta::createBodyLayer);
         event.registerLayerDefinition(RenderEnhancedBettaEgg.BETTA_EGG_LAYER, ModelEnhancedBettaEgg::createBodyLayer);
+    }
+
+    @SubscribeEvent
+    public static void colorSetup(ColorHandlerEvent.Block event) {
+        event.getBlockColors().register((p_92621_, p_92622_, p_92623_, p_92624_) -> {
+            return p_92622_ != null && p_92623_ != null ? lightenColor(BiomeColors.getAverageWaterColor(p_92622_, p_92623_)) : -1;
+        }, AddonBlocks.BUBBLE_NEST.get());
+    }
+
+    private static int lightenColor(int colorIn) {
+        int r = (3*((colorIn >> 16) & 255) + 255)/4;
+        int g = (3*((colorIn >> 8) & 255) + 255)/4;
+        int b = (3*((colorIn) & 255) + 255)/4;
+        return r << 16 | g << 8 | b;
     }
 
 }
