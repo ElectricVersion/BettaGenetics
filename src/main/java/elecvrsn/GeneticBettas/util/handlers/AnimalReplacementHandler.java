71f5bfff7388c3098deda8e9a7dbe0de38ca077b
ElectricVersion <electric.version01@gmail.com>
2025-04-10 01:40:10 -0500

    wandering traders now can bring betta buckets

--- src/main/java/elecvrsn/GeneticBettas/util/handlers/AnimalReplacementHandler.java
+++ src/main/java/elecvrsn/GeneticBettas/util/handlers/AnimalReplacementHandler.java
@@ -2,13 +2,22 @@ package elecvrsn.GeneticBettas.util.handlers;
 
 import elecvrsn.GeneticBettas.config.BettasCommonConfig;
 import elecvrsn.GeneticBettas.entity.EnhancedBetta;
+import elecvrsn.GeneticBettas.init.AddonItems;
+import mokiyoki.enhancedanimals.util.EanimodVillagerTrades;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.animal.AbstractFish;
+import net.minecraft.world.entity.npc.WanderingTrader;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 import static elecvrsn.GeneticBettas.init.AddonEntities.ENHANCED_BETTA;
 
@@ -31,7 +40,15 @@ public class AnimalReplacementHandler {
                         event.setCanceled(true);
                     }
                 }
+            } else if (entity instanceof WanderingTrader) {
+                if (!entity.getTags().contains("bettaTrades")) {
+                    entity.addTag("bettaTrades");
+                    if (ThreadLocalRandom.current().nextInt(10) == 9) {
+                        ((WanderingTrader) entity).getOffers().add(new MerchantOffer(new ItemStack(Items.EMERALD), new ItemStack(AddonItems.ENHANCED_BETTA_BUCKET.get()), 1, 2, 1F));
+                    }
+                }
             }
         }
     }
+
 }
