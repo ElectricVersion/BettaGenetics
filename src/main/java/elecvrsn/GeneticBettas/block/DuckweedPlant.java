adb1589a24a77ac7126f968cb9c1ef71b385c8b9
ElectricVersion <electric.version01@gmail.com>
2025-05-06 19:31:58 -0500

    can shear duckweed, can only place on water

--- src/main/java/elecvrsn/GeneticBettas/block/DuckweedPlant.java
+++ src/main/java/elecvrsn/GeneticBettas/block/DuckweedPlant.java
@@ -2,10 +2,15 @@ package elecvrsn.GeneticBettas.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.BushBlock;
@@ -18,6 +23,7 @@ import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
@@ -89,5 +95,20 @@ public class DuckweedPlant extends BushBlock implements BonemealableBlock {
 
     public BlockState getStateForPlacement(BlockPlaceContext context) {
         return this.defaultBlockState().setValue(PERSISTENT, true);
+    }
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
+        ItemStack stack = player.getItemInHand(hand);
+        if (stack.getItem() == Items.SHEARS && state.getValue(PERSISTENT)) {
+            level.setBlockAndUpdate(pos, state.setValue(PERSISTENT, false));
+            return InteractionResult.sidedSuccess(level.isClientSide);
+        }
+        return super.use(state, level, pos, player, hand, result);
+    }
+
+    public boolean canSurvive(BlockState state, LevelReader reader, BlockPos pos) {
+        BlockPos blockpos = pos.below();
+        if (state.getBlock() == this)
+            return reader.getFluidState(blockpos).getType() == Fluids.WATER;
+        return this.mayPlaceOn(reader.getBlockState(blockpos), reader, blockpos);
     }
 }
