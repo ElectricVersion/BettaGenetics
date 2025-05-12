9c1599c40e1deacfdb203ba3ad6c16136ab521a1
ElectricVersion <electric.version01@gmail.com>
2025-05-11 20:57:41 -0500

    reverted translation key to namespace geneticbettas

--- src/main/java/elecvrsn/GeneticBettas/entity/EnhancedBetta.java
+++ src/main/java/elecvrsn/GeneticBettas/entity/EnhancedBetta.java
@@ -80,7 +80,7 @@ import static elecvrsn.GeneticBettas.util.ColorUtil.getHSBFromHex;
 import static net.minecraft.world.level.block.Blocks.WATER;
 
 public class EnhancedBetta extends EnhancedAnimalAbstract implements Bucketable {
-    public static final String speciesTranslationKey = "entity.eanimod.enhanced_betta";
+    public static final String speciesTranslationKey = "entity.geneticbettas.enhanced_betta";
 
     protected static final ImmutableList<? extends SensorType<? extends Sensor<? super EnhancedBetta>>> SENSOR_TYPES = ImmutableList.of(SensorType.NEAREST_LIVING_ENTITIES, SensorType.NEAREST_ADULT, SensorType.HURT_BY, AddonSensorTypes.BETTA_ATTACKABLES.get(), AddonSensorTypes.BETTA_TRUSTABLES.get(), AddonSensorTypes.BETTA_FOOD_TEMPTATIONS.get());
     protected static final ImmutableList<? extends MemoryModuleType<?>> MEMORY_TYPES = ImmutableList.of(
