1da69468b076a195e14e2fbba8e7df5032f8f022
ElectricVersion <electric.version01@gmail.com>
2025-06-05 14:19:09 -0500

    changed plant spawns to individual rather than patches

--- src/main/java/elecvrsn/GeneticBettas/world/feature/AddonConfiguredFeatures.java
+++ src/main/java/elecvrsn/GeneticBettas/world/feature/AddonConfiguredFeatures.java
@@ -1,25 +1,19 @@
 package elecvrsn.GeneticBettas.world.feature;
 
 import elecvrsn.GeneticBettas.init.AddonBlocks;
-import elecvrsn.GeneticBettas.init.AddonFeatures;
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.data.worldgen.features.FeatureUtils;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicate;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.RandomPatchConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.SimpleBlockConfiguration;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
-import net.minecraft.world.level.material.Fluids;
 
 public class AddonConfiguredFeatures {
-    private static Holder<ConfiguredFeature<RandomPatchConfiguration, ?>> aquaticPlantFeature(String name, Block block) {
-        return FeatureUtils.register("patch_"+name, Feature.RANDOM_PATCH,
-                new RandomPatchConfiguration(8, 16, 0, PlacementUtils.filtered(AddonFeatures.AQUATIC_PLANT.get(),
-                        new SimpleBlockConfiguration(BlockStateProvider.simple(block)), BlockPredicate.matchesFluid(Fluids.WATER, BlockPos.ZERO))));
+    private static Holder<ConfiguredFeature<SimpleBlockConfiguration, ?>> aquaticPlantFeature(String name, Block block) {
+        return FeatureUtils.register("feature_"+name, Feature.SIMPLE_BLOCK, new SimpleBlockConfiguration(BlockStateProvider.simple(block)));
     }
 
     public static final Holder<ConfiguredFeature<RandomPatchConfiguration, ?>> PATCH_DUCKWEED =
@@ -27,10 +21,10 @@ public class AddonConfiguredFeatures {
                     new RandomPatchConfiguration(9, 2, 0, PlacementUtils.onlyWhenEmpty(Feature.SIMPLE_BLOCK,
                             new SimpleBlockConfiguration(BlockStateProvider.simple(AddonBlocks.DUCKWEED.get())))));
 
-    public static final Holder<ConfiguredFeature<RandomPatchConfiguration, ?>> PATCH_E_CORDIFOLIUS = aquaticPlantFeature("e_cordifolius", AddonBlocks.E_CORDIFOLIUS.get());
-    public static final Holder<ConfiguredFeature<RandomPatchConfiguration, ?>> PATCH_TALL_E_CORDIFOLIUS = aquaticPlantFeature("tall_e_cordifolius", AddonBlocks.TALL_E_CORDIFOLIUS.get());
-    public static final Holder<ConfiguredFeature<RandomPatchConfiguration, ?>> PATCH_A_BARTERI = aquaticPlantFeature("a_barteri", AddonBlocks.A_BARTERI.get());
-    public static final Holder<ConfiguredFeature<RandomPatchConfiguration, ?>> PATCH_VARIEGATED_A_BARTERI = aquaticPlantFeature("variegated_a_barteri", AddonBlocks.VARIEGATED_A_BARTERI.get());
-    public static final Holder<ConfiguredFeature<RandomPatchConfiguration, ?>> PATCH_V_AMERICANA = aquaticPlantFeature("v_americana", AddonBlocks.V_AMERICANA.get());
+    public static final Holder<ConfiguredFeature<SimpleBlockConfiguration, ?>> FEATURE_E_CORDIFOLIUS = aquaticPlantFeature("e_cordifolius", AddonBlocks.E_CORDIFOLIUS.get());
+    public static final Holder<ConfiguredFeature<SimpleBlockConfiguration, ?>> FEATURE_TALL_E_CORDIFOLIUS = aquaticPlantFeature("tall_e_cordifolius", AddonBlocks.TALL_E_CORDIFOLIUS.get());
+    public static final Holder<ConfiguredFeature<SimpleBlockConfiguration, ?>> FEATURE_A_BARTERI = aquaticPlantFeature("a_barteri", AddonBlocks.A_BARTERI.get());
+    public static final Holder<ConfiguredFeature<SimpleBlockConfiguration, ?>> FEATURE_VARIEGATED_A_BARTERI = aquaticPlantFeature("variegated_a_barteri", AddonBlocks.VARIEGATED_A_BARTERI.get());
+    public static final Holder<ConfiguredFeature<SimpleBlockConfiguration, ?>> FEATURE_V_AMERICANA = aquaticPlantFeature("v_americana", AddonBlocks.V_AMERICANA.get());
 
 }
--- src/main/java/elecvrsn/GeneticBettas/world/feature/AddonPlacedFeatures.java
+++ src/main/java/elecvrsn/GeneticBettas/world/feature/AddonPlacedFeatures.java
@@ -1,28 +1,22 @@
 package elecvrsn.GeneticBettas.world.feature;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicate;
 import net.minecraft.world.level.levelgen.placement.*;
 
 import java.util.List;
 
-import static net.minecraft.data.worldgen.placement.VegetationPlacements.worldSurfaceSquaredWithCount;
-import static net.minecraft.tags.BlockTags.DIRT;
-
 public class AddonPlacedFeatures {
     public static List<PlacementModifier> aquaticPlantPlacement(int rarity) {
-        return List.of(RarityFilter.onAverageOnceEvery(rarity), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP_OCEAN_FLOOR, BiomeFilter.biome());
+        return List.of(CountPlacement.of(2), RarityFilter.onAverageOnceEvery(rarity), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP_TOP_SOLID, BiomeFilter.biome());
     }
 
     public static final Holder<PlacedFeature> PATCH_DUCKWEED = PlacementUtils.register("patch_duckweed", AddonConfiguredFeatures.PATCH_DUCKWEED,
             RarityFilter.onAverageOnceEvery(16), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP_WORLD_SURFACE, BiomeFilter.biome());
 
-    public static final Holder<PlacedFeature> PATCH_E_CORDIFOLIUS = PlacementUtils.register("patch_e_cordifolius", AddonConfiguredFeatures.PATCH_E_CORDIFOLIUS, aquaticPlantPlacement(12));
-    public static final Holder<PlacedFeature> PATCH_TALL_E_CORDIFOLIUS = PlacementUtils.register("patch_tall_e_cordifolius", AddonConfiguredFeatures.PATCH_TALL_E_CORDIFOLIUS, aquaticPlantPlacement(12));
-    public static final Holder<PlacedFeature> PATCH_A_BARTERI = PlacementUtils.register("patch_a_barteri", AddonConfiguredFeatures.PATCH_A_BARTERI, aquaticPlantPlacement(12));
-    public static final Holder<PlacedFeature> PATCH_VARIEGATED_A_BARTERI = PlacementUtils.register("patch_variegated_a_barteri", AddonConfiguredFeatures.PATCH_VARIEGATED_A_BARTERI, aquaticPlantPlacement(48));
-    public static final Holder<PlacedFeature> PATCH_V_AMERICANA = PlacementUtils.register("patch_v_americana", AddonConfiguredFeatures.PATCH_V_AMERICANA, aquaticPlantPlacement(12));
+    public static final Holder<PlacedFeature> PATCH_E_CORDIFOLIUS = PlacementUtils.register("feature_e_cordifolius", AddonConfiguredFeatures.FEATURE_E_CORDIFOLIUS, aquaticPlantPlacement(4));
+    public static final Holder<PlacedFeature> PATCH_TALL_E_CORDIFOLIUS = PlacementUtils.register("feature_tall_e_cordifolius", AddonConfiguredFeatures.FEATURE_TALL_E_CORDIFOLIUS, aquaticPlantPlacement(6));
+    public static final Holder<PlacedFeature> PATCH_A_BARTERI = PlacementUtils.register("feature_a_barteri", AddonConfiguredFeatures.FEATURE_A_BARTERI, aquaticPlantPlacement(4));
+    public static final Holder<PlacedFeature> PATCH_VARIEGATED_A_BARTERI = PlacementUtils.register("feature_variegated_a_barteri", AddonConfiguredFeatures.FEATURE_VARIEGATED_A_BARTERI, aquaticPlantPlacement(48));
+    public static final Holder<PlacedFeature> PATCH_V_AMERICANA = PlacementUtils.register("feature_v_americana", AddonConfiguredFeatures.FEATURE_V_AMERICANA, aquaticPlantPlacement(6));
 }
